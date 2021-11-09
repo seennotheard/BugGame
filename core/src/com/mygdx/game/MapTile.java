0b4e127aed1d4128fbd2508f7f6e16b5ed23a17d
Sean Ma <seanm3266@gmail.com>
2021-11-09 14:08:04 -0500

    map placeholder stuff

---
+++ core/src/com/mygdx/game/MapTile.java
@@ -0,0 +1,12 @@
+package com.mygdx.game;
+
+public class MapTile { //class representing one map tile
+	String tileType = "";
+	int x;
+	int y;
+	
+	public void setType() {}
+	public String getType() {
+		return tileType;}
+	
+}
